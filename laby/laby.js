cbb8c6369eb14b38ad4cba688c22ca2c01a87498
nahuelfouilleul <nahuelfouilleul@yahoo.fr>
2025-12-25 23:19:39 +0100

    add mouse move

--- laby/laby.js
+++ laby/laby.js
@@ -1,30 +1,44 @@
 // global vars
 
 dots=[];
-rows=10;
-cols=18;
+rows=10; cols=18;
+//rows=5; cols=12;
 dots_space=48;
-move_length=24;
-dots_radius=16;
-line_width=32;
+move_length=12;
+dots_radius=12;
+line_width=24;
 endTime=null;
 
-rows*=2;
+/*rows*=2;
 cols*=2;
 dots_space/=2;
 move_length/=2;
 dots_radius/=2;
-line_width/=2;
+line_width/=2;*/
+
+goal=rows*cols-1;
+//goal=rows*cols-cols/2;
 
 for ( i=0; i<rows; i+=1 ) {
   for ( j=0; j<cols; j+=1) {
     ngbs=[];
     x=0;
-    if (i>0)      { ngbs[x++]=(i-1)*cols+j;}
+    if (i>0) { ngbs[x++]=(i-1)*cols+j;}
     if (i<rows-1) { ngbs[x++]=(i+1)*cols+j;}
-    if (j>0)      { ngbs[x++]=i*cols+j-1;}
-    if (j<cols-1) { ngbs[x++]=i*cols+j+1;}
+    if (j>0&&(i+j)%4>0)      { ngbs[x++]=i*cols+j-1; }
+    if (j<cols-1&&(i+j)%4<3) { ngbs[x++]=i*cols+j+1; }
+    /*if (i>1) { 
+      ngbs[x++]=i*cols+(cols+j-1)%cols;
+      ngbs[x++]=i*cols+(j+1)%cols;
+    }*/
+
+    if (i>0&&j>0&&i%2!=j%2&&(i+j+1)%4<2) { ngbs[x++]=(i-1)*cols+j-1;}
+    if (i<rows-1&&j<cols-1&&i%2!=j%2&&(i+j)%4<2) { ngbs[x++]=(i+1)*cols+j+1;}
+    //if (i>0&&j<cols-1&&i%2!=j%2&&(4*cols+i-j)%4<2) { ngbs[x++]=(i-1)*cols+j+1;}
+    //if (i<rows-1&&j>0&&i%2!=j%2&&(4*cols+i-j+1)%4<2) { ngbs[x++]=(i+1)*cols+j-1;}
+
     dots[i*cols+j]=[dots_space*(j+1),dots_space*(i+1),ngbs];
+    //dots[i*cols+j]=[500+dots_space*(i+1)*Math.cos(2*Math.PI*j/cols),300+dots_space*(i+1)*Math.sin(2*Math.PI*j/cols),ngbs];
   }
 }
 
@@ -73,15 +87,16 @@ function init() {
     }
   }
   
-  x = dots[dots.length-1][0];
-  y = dots[dots.length-1][1];
-  draw_player("#3f3");
+  x = dots[goal][0];
+  y = dots[goal][1];
+  draw_player("#3c3");
   
   x = dots[0][0];
   y = dots[0][1];
-  draw_player("#f33");
+  draw_player("#c33");
   
   document.body.onkeydown = canvas_onkeydown;
+  document.body.onmousemove = canvas_onmousemove;
   
 }
 function draw_player(c) {
@@ -94,42 +109,63 @@ function draw_player(c) {
   ctx.stroke();
 }
 coups=0;
-function canvas_onkeydown(event) {
-  if ( endTime ) {
-    return;
-  }
+
+function control_move(tx,ty) {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-  if (event.key === "ArrowUp" && ctx.getImageData(x,y-move_length,1,1).data[0]===102 ) {
-    draw_player("#ccc");
-    y -= move_length;
+  return Math.abs(ctx.getImageData(tx,ty,1,1).data[0]-125)<50;
+}
+
+function try_move(tx,ty) {
+  if (control_move(tx,ty)) {
+    draw_player("#678");
+    x=tx;
+    y=ty;
     coups += 1;
     draw_player("#f33");
-  } else if (event.key === "ArrowDown" && ctx.getImageData(x,y+move_length,1,1).data[0]===102 ) {
-    draw_player("#ccc");
-    y += move_length;
-    coups += 1;
-    draw_player("#f33");
-  } else if (event.key === "ArrowLeft" && ctx.getImageData(x-move_length,y,1,1).data[0]===102 ) {
-    draw_player("#ccc");
-    x -= move_length;
-    coups += 1;
-    draw_player("#f33");
-  } else if (event.key === "ArrowRight" && ctx.getImageData(x+move_length,y,1,1).data[0]===102 ) {
-    draw_player("#ccc");
-    x += move_length;
-    coups += 1;
-    draw_player("#f33");
-  }
-  if ( coups === 1 ) {
-    startTime=new Date();
+    if ( coups === 1 ) {
+      startTime=new Date();
+    }
   }
   document.getElementById("count").innerHTML=coups;
   document.getElementById("eltime").innerHTML=(new Date()-startTime)/1000;
   
-  if ( Math.abs(x-dots[dots.length-1][0])<move_length && Math.abs(y-dots[dots.length-1][1])<move_length ) {
+  if ( Math.abs(x-dots[goal][0])<move_length && Math.abs(y-dots[goal][1])<move_length*0.9 ) {
     endTime=new Date();
     document.getElementById("message").innerHTML="Bravo!";
   }
-} 
+}
+
+function canvas_onkeydown(event) {
+  if ( endTime ) {
+    return;
+  }
+  if (event.key === "ArrowUp") {
+    try_move(x,y-move_length);
+  } else if (event.key === "ArrowDown") {
+    try_move(x,y+move_length);
+  } else if (event.key === "ArrowLeft") {
+    try_move(x-move_length,y);
+  } else if (event.key === "ArrowRight") {
+    try_move(x+move_length,y);
+  }
+}
+
+function canvas_onmousemove(event) {
+  diffx = Math.abs(event.clientX-x);
+  diffy = Math.abs(event.clientY-y);
+  if (diffx>diffy) {
+    if (event.clientX>x) {
+      try_move(x+move_length,y);
+    } else {
+      try_move(x-move_length,y);
+    }
+  } else {
+    if (event.clientY>y) {
+      try_move(x,y+move_length);
+    } else {
+      try_move(x,y-move_length);
+    }  
+  }
+}
 
