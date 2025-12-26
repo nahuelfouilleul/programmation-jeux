e4ec6117e52f24707bee30b8344b333e59dee0cb
nahuelfouilleul <nahuelfouilleul@yahoo.fr>
2025-12-26 19:06:40 +0100

    fixes

--- laby/laby.js
+++ laby/laby.js
@@ -1,44 +1,70 @@
-// global vars
 
-dots=[];
-rows=10; cols=18;
-//rows=5; cols=12;
-dots_space=48;
-move_length=12;
-dots_radius=12;
-line_width=24;
-endTime=null;
+function init_dots(type,level) {
+  dots=[];
+ 
+  if (type<3) {
+    rows=10; cols=18;
+  } else {
+    rows=4; cols=12;
+  }
+  dots_space=48;
+  move_length=12;
+  dots_radius=9;
+  line_width=18;
+  endTime=null;
 
-/*rows*=2;
-cols*=2;
-dots_space/=2;
-move_length/=2;
-dots_radius/=2;
-line_width/=2;*/
+  if (level<2) {
+    rows/=2;
+    cols/=2;
+    dots_space*=2;
+    move_length*=2;
+    dots_radius*=2;
+    line_width*=2;
+  } else if (level>2) {
+    rows*=2;
+    cols*=2;
+    dots_space/=2;
+    move_length/=2;
+    dots_radius/=2;
+    line_width/=2;
+  }
 
-goal=rows*cols-1;
-//goal=rows*cols-cols/2;
+  if (type<3) {
+    goal=rows*cols-1;
+  } else {
+    goal=rows*cols-cols/2;
+  }
 
-for ( i=0; i<rows; i+=1 ) {
-  for ( j=0; j<cols; j+=1) {
-    ngbs=[];
-    x=0;
-    if (i>0) { ngbs[x++]=(i-1)*cols+j;}
-    if (i<rows-1) { ngbs[x++]=(i+1)*cols+j;}
-    if (j>0&&(i+j)%4>0)      { ngbs[x++]=i*cols+j-1; }
-    if (j<cols-1&&(i+j)%4<3) { ngbs[x++]=i*cols+j+1; }
-    /*if (i>1) { 
-      ngbs[x++]=i*cols+(cols+j-1)%cols;
-      ngbs[x++]=i*cols+(j+1)%cols;
-    }*/
+  for ( i=0; i<rows; i+=1 ) {
+    for ( j=0; j<cols; j+=1) {
+      ngbs=[];
+      x=0;
+ 
+      if (i>0 && (type <3 || j%4==i%4) ) { ngbs[x++]=(i-1)*cols+j;}
+      if (i<rows-1 && (type <3 || j%4==(i+1)%4)) { ngbs[x++]=(i+1)*cols+j;}
+      if (type<3) {
+        if (j>0&&(i+j)%4>0)      { ngbs[x++]=i*cols+j-1; }
+        if (j<cols-1&&(i+j)%4<3) { ngbs[x++]=i*cols+j+1; }
+      }else {
+        //if (level<2) {
+          ngbs[x++]=i*cols+(cols+j-1)%cols;
+          ngbs[x++]=i*cols+(j+1)%cols;
+        //}
+      }
 
-    if (i>0&&j>0&&i%2!=j%2&&(i+j+1)%4<2) { ngbs[x++]=(i-1)*cols+j-1;}
-    if (i<rows-1&&j<cols-1&&i%2!=j%2&&(i+j)%4<2) { ngbs[x++]=(i+1)*cols+j+1;}
-    //if (i>0&&j<cols-1&&i%2!=j%2&&(4*cols+i-j)%4<2) { ngbs[x++]=(i-1)*cols+j+1;}
-    //if (i<rows-1&&j>0&&i%2!=j%2&&(4*cols+i-j+1)%4<2) { ngbs[x++]=(i+1)*cols+j-1;}
-
-    dots[i*cols+j]=[dots_space*(j+1),dots_space*(i+1),ngbs];
-    //dots[i*cols+j]=[500+dots_space*(i+1)*Math.cos(2*Math.PI*j/cols),300+dots_space*(i+1)*Math.sin(2*Math.PI*j/cols),ngbs];
+      if (type==2){
+        if (i>0&&j>0&&i%2!=j%2&&(i+j+1)%4<2) { ngbs[x++]=(i-1)*cols+j-1;}
+        if (i<rows-1&&j<cols-1&&i%2!=j%2&&(i+j)%4<2) { ngbs[x++]=(i+1)*cols+j+1;}
+        if (i>0&&j<cols-1&&i%2!=j%2&&(4*cols+i-j)%4<2) { ngbs[x++]=(i-1)*cols+j+1;}
+        if (i<rows-1&&j>0&&i%2!=j%2&&(4*cols+i-j+1)%4<2) { ngbs[x++]=(i+1)*cols+j-1;}
+      }
+ 
+      if (type<3) {
+        dots[i*cols+j]=[dots_space*(j+.5),dots_space*(i+.5),ngbs];
+      } else {
+        dots[i*cols+j]=[500+dots_space*(i+2)*Math.cos(2*Math.PI*j/cols),300+dots_space*(i+2)*Math.sin(2*Math.PI*j/cols),ngbs];
+      }
+    }
   }
 }
 
@@ -50,7 +76,7 @@ function generate() {
   ngb_set = new Set(dots[rand_dot][2]);
   dots[rand_dot][2]=[];
   int_arr = Array.from( ngb_set.intersection( hash_set ) );
-  
+ 
   while (int_arr.length>0) {
     rand_ngb = int_arr[Math.floor(Math.random()*int_arr.length)];
     hash_set.delete(rand_ngb);
@@ -61,19 +87,23 @@ function generate() {
     dots[rand_ngb][2]=[ tmp_arr[Math.floor(Math.random()*tmp_arr.length)] ];
     int_arr = Array.from( ngb_set.intersection( hash_set ) );
   }
-  
+ 
 }
 
 function init() {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-  
+ 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#654";
-  ctx.strokeStyle = "#654";
-  
+  ctx.fillStyle = "#555";
+  ctx.strokeStyle = "#555";
+ 
+  startTime=endTime=null;
+  coups=0;
+
+  init_dots(document.getElementById('type').value,document.getElementById('level').value);
   generate();
-  
+ 
   for ( dot of dots ) {
     ctx.beginPath();
     ctx.arc(dot[0], dot[1], dots_radius, 0, 2 * Math.PI);
@@ -86,30 +116,34 @@ function init() {
       ctx.stroke();
     }
   }
-  
+ 
   x = dots[goal][0];
   y = dots[goal][1];
-  draw_player("#3c3");
-  
+  draw_player("#2f2");
+ 
   x = dots[0][0];
   y = dots[0][1];
-  draw_player("#c33");
-  
+  draw_player("#f22");
+ 
   document.body.onkeydown = canvas_onkeydown;
+  document.body.onkeypress = canvas_onkeydown;
+  document.body.onkeyup = canvas_onkeydown;
+  document.body.onmousedown = canvas_onmousemove;
   document.body.onmousemove = canvas_onmousemove;
+  document.body.onmouseup = canvas_onmousemove;
+  document.body.ontouchmove = canvas_onmousemove;
   document.body.ontouchstart = canvas_onmousemove;
-  
+ 
 }
 function draw_player(c) {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-  ctx.strokeStyle = c; 
+  ctx.strokeStyle = c;
   ctx.lineWidth = 2;
   ctx.beginPath();
   ctx.arc(x, y, dots_radius*0.7, 0, 2 * Math.PI);
   ctx.stroke();
 }
-coups=0;
 
 function control_move(tx,ty) {
   const canvas = document.getElementById("canvas");
@@ -119,18 +153,20 @@ function control_move(tx,ty) {
 
 function try_move(tx,ty) {
   if (control_move(tx,ty)) {
-    draw_player("#678");
+    draw_player("#dd2");
     x=tx;
     y=ty;
     coups += 1;
-    draw_player("#f33");
+    draw_player("#f22");
     if ( coups === 1 ) {
       startTime=new Date();
     }
   }
   document.getElementById("count").innerHTML=coups;
-  document.getElementById("eltime").innerHTML=(new Date()-startTime)/1000;
-  
+  if (startTime) {
+    document.getElementById("eltime").innerHTML=(new Date()-startTime)/1000;
+  }
+ 
   if ( Math.abs(x-dots[goal][0])<move_length && Math.abs(y-dots[goal][1])<move_length*0.9 ) {
     endTime=new Date();
     document.getElementById("message").innerHTML="Bravo!";
@@ -153,20 +189,25 @@ function canvas_onkeydown(event) {
 }
 
 function canvas_onmousemove(event) {
-  diffx = Math.abs((event.clientX||event.touches[0].clientX)-x);
-  diffy = Math.abs((event.clientY||event.touches[0].clientY)-y);
-  if (diffx>diffy) {
-    if (event.clientX>x) {
-      try_move(x+move_length,y);
-    } else {
-      try_move(x-move_length,y);
-    }
-  } else {
-    if (event.clientY>y) {
-      try_move(x,y+move_length);
-    } else {
-      try_move(x,y-move_length);
-    }  
+  if ( endTime ) {
+    return;
   }
+  ex = event.clientX || event.touches[0].clientX;
+  ey = event.clientY || event.touches[0].clientY;
+  document.getElementById('ex').innerHTML=ex;
+  document.getElementById('ey').innerHTML=ey;
+  if (!ex || !ey || ex > canvas.width || ey > canvas.height) {
+    return;
+  }
+  if (ex>x) {
+    try_move(x+move_length,y);
+  } else {
+    try_move(x-move_length,y);
+  }
+  if (ey>y) {
+    try_move(x,y+move_length);
+  } else {
+    try_move(x,y-move_length);
+  } 
 }
 
